5c8f8cf0d3caa101b993bd9466be267f1bed7de7
Darwin <piksel48@gmail.com>
2025-04-17 01:44:21 +0200

    Hotfix oxidise.sh

--- setup/oxidise.sh
+++ setup/oxidise.sh
@@ -5,22 +5,7 @@ __cmd() {
 __cmd cargo || ~/.files/setup/rust.sh
 
 cargo install sccache
-echo export RUSTC_WRAPPER=sccache >> ~/.zprofile
-RUSTC_WRAPPER=sccache cargo install \
-  cargo-info \ # better cargo info
-  cargo-watch \ # ???
-  nu \ # modern shell /w structured pipelines
-  ripgrep \ # better grep
-  du-dust \ # better du
-  exa \ # better ls
-  bat \ # better cat
-  irust \ # rust interpreter
-  bacon \ # rust background compiler
-  porsmo \ # pomodoro
-  speedtest-rs \ # speedtest
-  wiki-tui \ # wikipedia terminal ui
-  mrpocs \ # single proc monitor
-  rtx-cli \ # env-ver final manager
-  # zellij \ # terminal muxer
-  # gitui \ # git terminal ui (seems broken on mac)
-  # ncspot \ # spotify terminal ui
+[ -n "$RUSTC_WRAPPER" ] && echo export RUSTC_WRAPPER=sccache >> ~/.zprofile
+RUSTC_WRAPPER=sccache cargo install cargo-info cargo-watch nu ripgrep \
+  du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli \
+  zellij gitui ncspot
