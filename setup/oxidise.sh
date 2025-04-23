c055dbadb1910f0050e9503d9c60100d1abce059
Darwin <piksel48@gmail.com>
2025-04-23 16:27:07 +0200

    Update oxidation

--- setup/oxidise.sh
+++ setup/oxidise.sh
@@ -6,6 +6,6 @@ __cmd cargo || ~/.files/setup/rust.sh
 
 cargo install sccache
 [ -n "$RUSTC_WRAPPER" ] && echo export RUSTC_WRAPPER=sccache >> ~/.zprofile
-RUSTC_WRAPPER=sccache cargo install cargo-info cargo-watch nu ripgrep \
+RUSTC_WRAPPER=sccache cargo install cargo-info nu ripgrep \
   du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli \
-  zellij gitui ncspot
+  zellij gitui ncspot hd xh
