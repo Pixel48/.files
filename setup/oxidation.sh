92e7afdc486489a88f4c384f90f81ea75b1d7ffe
Darwin <piksel48@gmail.com>
2025-04-23 19:10:26 +0200

    Merge crates in one line

--- setup/oxidation.sh
+++ setup/oxidation.sh
@@ -6,6 +6,4 @@ __cmd cargo || ~/.files/setup/rust.sh
 
 cargo install sccache
 [ -n "$RUSTC_WRAPPER" ] && echo export RUSTC_WRAPPER=sccache >> ~/.zprofile
-RUSTC_WRAPPER=sccache cargo install cargo-info nu ripgrep \
-  du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli \
-  zellij gitui ncspot hd xh
+RUSTC_WRAPPER=sccache cargo install cargo-info nu ripgrep du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli zellij gitui ncspot hd xh
