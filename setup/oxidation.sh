cc11fe6859132117042a7b80e04e0bd963dd33f3
Moonstone <pixel48@gmail.com>
2025-05-09 00:17:05 +0200

    Refactor oxidation.sh

--- setup/oxidation.sh
+++ setup/oxidation.sh
@@ -5,5 +5,6 @@ __cmd() {
 __cmd cargo || ~/.files/setup/rust.sh
 
 __cmd sccache || cargo install --locked sccache
-[ -n "$RUSTC_WRAPPER" ] && echo export RUSTC_WRAPPER=sccache >> ~/.zprofile
-RUSTC_WRAPPER=sccache cargo install --locked cargo-info nu ripgrep du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli zellij gitui ncspot hd xh zoxide
+[ -n "$RUSTC_WRAPPER" ] && echo 'export RUSTC_WRAPPER=sccache' >> ~/.zprofile
+
+RUSTC_WRAPPER=sccache cargo install --locked cargo-info nu ripgrep du-dust exa bat irust bacon porsmo speedtest-rs wiki-tui mrpocs rtx-cli zellij gitui hd xh zoxide
