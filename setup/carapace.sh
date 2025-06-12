a8b0dff695ac4bd8d7322fe943162140b5f937e9
Moonstone <pixel48@gmail.com>
2025-06-13 00:36:58 +0200

    Add carapace setup

---
+++ setup/carapace.sh
@@ -0,0 +1,6 @@
+zypper ar --gpgcheck-allow-unsigned -f https://yum.fury.io/rsteube/ carapace
+zypper install -y carapace-bin
+
+echo """export CARAPACE_BRIDGES='zsh,fish,bash,inshellisense' # optional
+zstyle ':completion:*' format $'\e[2;37mCompleting %d\e[m'
+source <(carapace _carapace)""" >> ~/.zprofile
