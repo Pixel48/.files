ce228120cef6a0906ef1fa22f63aa031cffbd629
Darwin <piksel48@gmail.com>
2025-08-11 20:32:07 +0200

    Add yaml to nvim

---
+++ .config/nvim/lua/plugins/config/yaml.lua
@@ -0,0 +1,10 @@
+---@type NvPluginSpec
+return {
+  "cuducos/yaml.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "folke/snacks.nvim",
+    "nvim-telescope/telescope.nvim",
+    "ibhagwan/fzf-lua",
+  },
+}
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
-  { import = 'plugins.config.mason' },
-  { import = 'plugins.config.lspconfig' },
-  { import = 'plugins.config.conform' },
-  { import = 'plugins.config.rust' },
-  { import = 'plugins.config.kualala' },
-  { import = 'plugins.config.platformio'},
+  { import = "plugins.config.mason" },
+  { import = "plugins.config.lspconfig" },
+  { import = "plugins.config.conform" },
+  { import = "plugins.config.rust" },
+  { import = "plugins.config.kualala" },
+  { import = "plugins.config.platformio" },
   -- { import = 'plugins.config.terraform'},
-  { import = 'plugins.config.tree-sitter'},
+  { import = "plugins.config.tree-sitter" },
+  { import = "plugins.config.yaml" },
 }
