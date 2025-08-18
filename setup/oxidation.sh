57132749bac431cf01b7a596fefdcd70672377f4
Moonstone <pixel48@gmail.com>
2025-08-19 00:04:10 +0200

    Add git-ignore-generator to oxidation

--- setup/oxidation.sh
+++ setup/oxidation.sh
@@ -7,4 +7,4 @@ __cmd cargo || ~/.files/setup/rust.sh
 __cmd sccache || cargo install sccache
 [ -n "$RUSTC_WRAPPER" ] && echo 'export RUSTC_WRAPPER=sccache' >> ~/.profile
 
-RUSTC_WRAPPER=sccache cargo install --locked cargo-info nu ripgrep du-dust eza bat irust bacon porsmo speedtest-rs wiki-tui rtx-cli hd xh zoxide hyperfine fselect ncspot tokei just mask presenterm tealdeer open
+RUSTC_WRAPPER=sccache cargo install --locked cargo-info nu ripgrep du-dust eza bat irust bacon porsmo speedtest-rs wiki-tui rtx-cli hd xh zoxide hyperfine fselect ncspot tokei just mask presenterm tealdeer open git-ignore-generator
