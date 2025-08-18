6803f508f6d8761cbd6bf59245bab23ee7d5ad18
Moonstone <pixel48@gmail.com>
2025-08-19 01:35:05 +0200

    Update nvim-ufo

--- .config/nvim/lua/plugins/config/ufo.lua
+++ .config/nvim/lua/plugins/config/ufo.lua
@@ -5,6 +5,8 @@ vim.o.foldenable = true
 
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+vim.keymap.set("n", "zm", require("ufo").closeFoldsWith)
 vim.keymap.set("n", "zv", function()
   local winid = require("ufo").peekFoldedLinesUnderCursor()
   if not winid then
