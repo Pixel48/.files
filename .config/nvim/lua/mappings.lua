fd9302613feb05e56f9b27c5c6df50945a4b9bad
Moonstone <pixel48@gmail.com>
2025-10-13 16:07:43 +0200

    Add nvim exit terminal mode mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,10 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("t", "<C-Space>", "<C-x>", { remap = true, silent = true })
+map("t", "<C-@>", "<C-x>", { remap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
