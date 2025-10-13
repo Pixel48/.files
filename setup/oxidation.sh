1f793291ac434ef74211e928a688a8fc4b83cba7
Darwin <piksel48@gmail.com>
2025-10-13 16:15:01 +0200

    Remove doubled ncspot from oxidation.sh

--- setup/oxidation.sh
+++ setup/oxidation.sh
@@ -7,4 +7,4 @@ __cmd cargo || ~/.files/setup/rust.sh
 __cmd sccache || cargo install sccache
 [ -n "$RUSTC_WRAPPER" ] && echo 'export RUSTC_WRAPPER=sccache' >> ~/.profile
 
-RUSTC_WRAPPER=sccache cargo install --locked cargo-info ripgrep ripgrep-all du-dust eza bat irust bacon porsmo speedtest-rs wiki-tui rtx-cli hd xh zoxide hyperfine fselect ncspot tokei just mask presenterm tealdeer open git-ignore-generator fd-find mprocs rusty-man ncspot
+RUSTC_WRAPPER=sccache cargo install --locked cargo-info ripgrep ripgrep-all du-dust eza bat irust bacon porsmo speedtest-rs wiki-tui rtx-cli hd xh zoxide hyperfine fselect tokei just mask presenterm tealdeer open git-ignore-generator fd-find mprocs rusty-man ncspot
