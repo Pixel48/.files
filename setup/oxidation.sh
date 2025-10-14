f80bafdb375f713828e7be421bb1b898d92191b0
Darwin <piksel48@gmail.com>
2025-10-14 13:33:24 +0200

    Lock sccache installation

--- setup/oxidation.sh
+++ setup/oxidation.sh
@@ -4,7 +4,7 @@ __cmd() {
 
 __cmd cargo || ~/.files/setup/rust.sh
 . ~/.profile
-__cmd sccache || cargo install sccache
+__cmd sccache || cargo install --locked sccache
 [ -n "$RUSTC_WRAPPER" ] && echo 'export RUSTC_WRAPPER=sccache' >> ~/.profile
 
 RUSTC_WRAPPER=sccache cargo install --locked cargo-info ripgrep ripgrep-all du-dust eza bat irust bacon porsmo speedtest-rs wiki-tui rtx-cli hd xh zoxide hyperfine fselect tokei just mask presenterm tealdeer open git-ignore-generator fd-find mprocs rusty-man ncspot
