686d5858dc5c8842e39666b312820607f878ed4b
Moonstone <pixel48@gmail.com>
2026-01-26 21:34:40 +0100

    Bind ^j/^k to scroll

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -7,4 +7,8 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 
 map("t", "<C-Space>", "<C-x>", { remap = true, silent = true })
 map("t", "<C-@>", "<C-x>", { remap = true, silent = true })
+
+-- Scroll buffer by line
+map("n", "<C-j>", "<C-e>", { desc = "Scroll down one line" })
+map("n", "<C-k>", "<C-y>", { desc = "Scroll up one line" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
