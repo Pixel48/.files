b4a080d046db1697668c40e48041f3e07ac7115e
Moonstone <pixel48@gmail.com>
2026-02-26 07:43:49 +0100

    Rework pipe_install script

--- setup/.pipe_install.sh
+++ setup/.pipe_install.sh
@@ -13,12 +13,29 @@ warn() { echo -e "\e[33m$*\e[m"; }
 error() { echo -e "\e[31m$*\e[m" >&2; exit 1; }
 fatal() { echo -e "\e[07;91m$*\e[m"; }
 
-curl -fsSL "$@" > installer.sh
+if ! curl -fsSL "$@" > installer.sh; then
+  error "Failed to download installer from: $@"
+fi
+
+if [ ! -s installer.sh ]; then
+  error "Downloaded installer is empty"
+fi
+
 command -v bat >/dev/null && bat installer.sh || cat installer.sh
-read -p "Approve? [N/y] " install
-if [[ -z $install || $install != [yY]* ]]; then
-  warn "Reverting"
-  exit 0
+
+auto_approve=false
+for arg in "$@"; do
+  case $arg in
+    -y|--yes|--assume-yes) auto_approve=true ;;
+  esac
+done
+
+if [ "$auto_approve" = false ]; then
+  read -p "Approve? [N/y] " install
+  if [[ -z $install || $install != [yY]* ]]; then
+    warn "Reverting"
+    exit 0
+  fi
 fi
 
 chmod +x installer.sh
