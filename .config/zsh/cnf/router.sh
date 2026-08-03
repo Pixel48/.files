315ed4fd30d8d5ed23f4aa49487c6db9d182cda5
Moonstone <pixel48@gmail.com>
2026-08-03 12:44:45 +0200

    Remove junk comments from router

--- .config/zsh/cnf/router.sh
+++ .config/zsh/cnf/router.sh
@@ -1,16 +1,11 @@
-# --- AUTO-DISCOVERY DLA COMMAND_NOT_FOUND ---
 local plugin_dir="$HOME/.config/zsh/cnf/plugins"
 typeset -a cnf_plugins=()
 
 if [[ -d "$plugin_dir" ]]; then
-  # 1. Wczytujemy pliki z obu rozszerzeń, nie przejmując się bałaganem
   for plugin_file in "$plugin_dir"/*.zsh(N) "$plugin_dir"/*.sh(N); do
     source "$plugin_file"
   done
   
-  # 2. Bezpieczna introspekcja. 
-  # MAGIA: (onk) oznacza Order Ascending, Numeric, Keys.
-  # Dzięki literce 'n', funkcja cnf_100_... zawsze będzie po cnf_90_...
   for func in ${(onk)functions}; do
     if [[ "$func" == cnf_* ]]; then
       cnf_plugins+=("$func")
